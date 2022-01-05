18b10b7127f6642a8b87c31dd0f951b7a1e2d257
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-01-05 17:54:39 +0000

    Complete oldestPackageName task.

--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -27,9 +27,35 @@ The results should have this structure:
  *  With the results from this request, inside "content", return
  *  the "name" of the package that has the oldest "date" value
  */
+const fetch = require('node-fetch');
 
 module.exports = async function oldestPackageName() {
-  // TODO
+  const fetchUrl = 'http://ambush-api.inyourarea.co.uk/ambush/intercept';
 
-  return name
+  const fetchBody = {
+    url: 'https://api.npms.io/v2/search/suggestions?q=react',
+    method: 'GET',
+    return_payload: true,
+  };
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(fetchBody),
+  };
+
+  const res = await fetch(fetchUrl, fetchOptions);
+  const data = await res.json();
+  const contentItems = data.content;
+  const givenPackages = contentItems.map(item => item.package);
+
+  const oldestPackage = givenPackages.reduce((prev, curr) =>
+    curr.date < prev.date ? curr : prev,
+  );
+
+  const name = oldestPackage.name;
+
+  return name;
 };
